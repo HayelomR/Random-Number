b50de8132deb6480939f6d944fb1f6500b899a95
HayelomR <rhayelom@gmail.com>
2017-05-24 09:36:41 -0600

    inital commit

---
+++ assets/javascript/games.js
@@ -0,0 +1,12 @@
+$(document).ready(function() { // when the 
+var win = 0;
+var loss = 0;
+var counter = 0;
+
+// lets create a random number that will generate betwen 19 & 120
+  var Random=Math.floor(Math.random()*120+19) 
+
+
+
+
+}); //parenthesis that end the function
