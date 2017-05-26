2b06a56658fbc33f12b62f3803cb3e72239fabef
HayelomR <rhayelom@gmail.com>
2017-05-25 18:24:46 -0600

    added images

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+//player should guess a number
+//there will be four crystals displayed on the page.
+//when crystals clicked they will add a specific amount of points
+//player when total score == randomnumber 
+
+
+$(document).ready(function() { // when the 
+   var win = 0;
+	var loss = 0;
+	var counter = 0;
+
+// lets create a random number that will generate betwen 19 & 120
+function initalizeGame (){
+	var Random=Math.floor(Math.random()*120+19) 
+	console.log();
+
+
+
+
+
+}
+}); //parenthesis that end the function
